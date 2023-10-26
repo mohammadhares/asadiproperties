6aab4c7c280c8f5410b700f3bef1a0e2c3c99de8
Haris <mharis@tibxtech.com>
2023-10-26 17:57:37 +0430

    Add funcitons to website controller

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class WebsiteController extends Controller
 {
-    //
+    public function home() {
+        return view('index');
+    }
+    public function about() {
+        return view('about');
+    }
+    public function blog() {
+        return view('blog');
+    }
+    public function offplane() {
+        return view('offplane');
+    }
+    public function contact() {
+        return view('contact');
+    }
+    public function rent() {
+        return view('rent');
+    }
+    public function sale() {
+        return view('sale');
+    }
 }
