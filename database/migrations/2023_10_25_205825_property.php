bdeac60c88c74596c2ce7872b2f4d0444d94f94e
Haris <mharis@tibxtech.com>
2023-10-26 17:36:06 +0430

    Add Migrations and Database configurations

---
+++ database/migrations/2023_10_25_205825_property.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('address');
+            $table->double('longitude');
+            $table->double('latitude');
+            $table->integer('price');
+            $table->integer('size');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->date('year_built');
+            $table->text('features');
+            $table->string('property_type');
+            $table->string('status');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
---
+++ database/migrations/2023_10_25_212117_projects.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('developer');
+            $table->string('address');
+            $table->double('longitude');
+            $table->double('latitude');
+            $table->integer('price');
+            $table->date('payment_plans');
+            $table->text('features');
+            $table->string('project_type');
+            $table->string('status');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
